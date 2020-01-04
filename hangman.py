a7e0d8efbea94ed10122571e8853e166af1b4843
patty <59509719+patty111@users.noreply.github.com>
2020-01-05 00:29:16 +0800

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,50 @@
+import random_words as rw
+from module import aligorithm
+
+while True:
+    word = rw.RandomWords().random_word()
+    if word[-1] != 's' and len(word) > 5:
+        break
+
+chance=6
+ansBox = ['_' for i in range(len(word))]
+word=[i for i in word]
+usedWords = set({})
+
+print(' '.join(ansBox))
+
+while chance != 0:
+    print('%s chances left'%chance)
+    OK = True
+    while OK:
+        guess = input('Enter a character : ')
+        if guess.isalpha() == True:
+            OK = False
+        else:
+            print('Please do not enter numbers or other signs ')
+    usedWords.add(guess)
+
+    if guess in word:
+        index = aligorithm.countIndex(word,guess)
+        for i in index:
+            ansBox[i] = guess
+    else:
+        chance-=1
+    print(' '.join(ansBox))
+    wordslist = [i for i in usedWords]
+    print(','.join(wordslist))
+
+    count = 0
+    
+    for i in ansBox:
+        if i != '_':
+            count+=1
+    
+    if count == len(word):
+        print('\nCongrats ! You did it !')
+        break
+
+if chance == 0 and count != len(word):
+    print('\nYou Lost @@')
+
+print('\n'+''.join(word))
